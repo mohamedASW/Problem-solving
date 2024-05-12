8216e71380ef150b6cefcc366325e831e00538ca
mohamed ahmed <mohamed@ahmed>
2024-05-12 23:43:46 +0300

    problems on binary search algorithims save last changes

--- App/BinarySearch.cs
+++ App/BinarySearch.cs
@@ -7,8 +7,14 @@ using System.Xml.Serialization;
 namespace Problemsolving;
 public static class BinarySearch
 {
-    // get number of items that you can buy using binary search algorithim 
-   public static int Get_Items_Count(int[] prices , int maxcoins)
+   
+    /// <summary>
+    /// get number of items that you can buy using binary search algorithim 
+    /// </summary>
+    /// <param name="prices"></param>
+    /// <param name="maxcoins"></param>
+    /// <returns></returns>
+    public static int Get_Items_Count(int[] prices , int maxcoins)
     {
         Array.Sort(prices);
         int l = 0;
@@ -30,14 +36,17 @@ public static class BinarySearch
         }
         return ans;
     }
-   
+
     /// <summary>
-    /// Get maximum median
+    /// Get maximum median <paramref name="arr"/> represent the integers <paramref name="k"/> max count of one’s
     /// </summary>
+    /// <remarks>
+    /// this implemntation its time complixity = Knlog(n)
+    /// </remarks>
     /// <param name="arr"></param>
     /// <param name="k"></param>
-    /// <returns></returns>
-    public static int Method(int[] arr , int k)
+    /// <returns>maximum median</returns>
+    public static int GetMaxmedianV1(int[] arr , int k)
     {
         Array.Sort(arr);
         
@@ -54,8 +63,32 @@ public static class BinarySearch
 
     }
 
-    public static int method2(int[] arr, int k )
+
+    /// <summary>
+    /// Get maximum median <paramref name="arr"/> represent the integers <paramref name="k"/> max count of one’s
+    /// </summary>
+    /// <remarks>
+    /// this implemntation its time complixity = nlog(n)
+    /// </remarks>
+    /// <param name="arr"></param>
+    /// <param name="k"></param>
+    /// <returns>maximum median</returns>
+    public static int GetMaxmedianV2(int[] arr, int k )
     {
+       bool can(int mid, int[] arr, int k)
+        {
+            int sum = 0;
+            int i = (arr.Length - 1) / 2;
+            while (i < arr.Length)
+            {
+                if (arr[i] < mid)
+                    sum += mid - arr[i];
+                i++;
+
+            }
+            return sum <= k;
+        }
+
         Array.Sort(arr);
         var mid = (arr.Length - 1) / 2;
         var minMadient= arr[mid];
@@ -75,31 +108,39 @@ public static class BinarySearch
         return ans;
     }
 
-    private static bool can(int mid , int[] arr , int k)
+
+
+    
+
+    /// <summary>
+    /// guess Smallest number achieve this condition v + v/<paramref name="k"/>+ v/<paramref name="k"/>^2 + v/<paramref name="k"/>^3 + ... +v/<paramref name="k"/>^p >= <paramref name="n"/> till v/<paramref name="k"/>^p equals Zero
+    /// </summary>
+    /// <remarks>
+    /// this number is v which divided by <paramref name="k"/> every time 
+    /// </remarks>
+    /// <param name="n"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public static int  GuessV(int n , int k)
     {
-        int sum = 0;
-        int i = (arr.Length - 1) / 2;
-        while (i < arr.Length)
+         bool can(int mid, int n, int k)
         {
-            if (arr[i]<mid)
-                sum += mid - arr[i];
-            i++;
-
+            int sum = mid;
+            while (mid != 0)
+            {
+                sum += mid / k;
+                mid /= k;
+            }
+            return sum >= n;
         }
-        return sum <= k;
-    }
 
-
-    // guess closest number to n 
-    public static int  method3(int n , int k)
-    {
         int l = 1, r = n;
         int ans = n;
         int mid; 
         while (l <= r)
         {
             mid = (l + r) / 2; 
-            if (can3(mid,n,k))
+            if (can(mid,n,k))
             {
                 ans = mid;
                 r = mid - 1;
@@ -113,19 +154,13 @@ public static class BinarySearch
         return ans; 
     }
 
-    private static bool can3(int mid, int n, int k)
-    {
-        int sum = mid; 
-        while (mid!=0)
-        {
-            sum += mid / k;
-            mid /=k;
-        }
-        return sum>=n;
-    }
 
-
-    public static int whogetcandie(int candiesamount)
+    /// <summary>
+    /// you have <paramref name="candiesamount"/> you have to destribute candies on the maximum number of childern on condition everyone get distinct amount  
+    /// </summary>
+    /// <param name="candiesamount"></param>
+    /// <returns></returns>
+    public static int whogetcandy(int candiesamount)
     {
         int l = 1,  r = candiesamount;
         int ans = 1;
--- App/Program.cs
+++ App/Program.cs
@@ -5,15 +5,15 @@
 //Console.WriteLine(BinarySearch.Get_Items_Count(new int[] {3,10,8,6,11 },3));
 //Console.WriteLine(BinarySearch.Get_Items_Count(new int[] {3,10,8,6,11 },11));
 
-//Console.WriteLine(BinarySearch.method2(new int[]{1,3,5 },2));
-//Console.WriteLine(BinarySearch.method2(new int[]{1,2,1,1,1 },5));
-//Console.WriteLine(BinarySearch.method2(new int[]{4,1,2,4,3,4,4 },7));
+//Console.WriteLine(BinarySearch.GetMaxmedianV2(new int[]{1,3,5 },2));
+//Console.WriteLine(BinarySearch.GetMaxmedianV2(new int[]{1,2,1,1,1 },5));
+//Console.WriteLine(BinarySearch.GetMaxmedianV2(new int[]{4,1,2,4,3,4,4 },7));
 
 
-//Console.WriteLine(BinarySearch.method3(7, 2));
-//Console.WriteLine(BinarySearch.method3(59, 9));
+//Console.WriteLine(BinarySearch.GuessV(7, 2));
+//Console.WriteLine(BinarySearch.GuessV(59, 9));
 
 
 
-Console.WriteLine(BinarySearch.whogetcandie(7));
+Console.WriteLine(BinarySearch.whogetcandy(7));
 
